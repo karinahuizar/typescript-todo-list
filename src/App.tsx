80213052edb0f23e1ab9e2eb20a4fafa32bc1218
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 11:08:16 -0500

    Created state for todo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 const App: React.FC = () => {
+  const [todo, useTodo] = useState("");
   return (
     <div className="App">
       <span className="heading">Taskify</span>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 
 const InputField = () => {
+
     return <form className='input'>
         <input type='input' placeholder='Enter a task!' className='input__box' />
         <button className='input_submit' type="submit">Go!</button>
