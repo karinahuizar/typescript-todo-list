83ab90ec9ef3ec9c19594396493a5b5fe86b7e6f
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 14:51:31 -0500

    Created SingleTodo component.

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Todo } from "../model";
+
+type Props = {
+    todo: Todo,
+    todos: Todo[],
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+    return (
+        <form className="todos__single">
+            <span className="todos__single--text">
+                {todo.todo}
+            </span>
+            <div>
+                <span className="icon"></span>
+                <span className="icon"></span>
+                <span className="icon"></span>
+            </div>
+        </form>
+    );
+
+};
+
+export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 import './styles.css';
 
 interface Props {
@@ -7,12 +8,14 @@ interface Props {
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC = ({ todos, setTodos }: Props) => {
-    return <div className="todos">
-        {todos.map(todo => (
-            <li>{todo.todo}</li>
-        ))}
-    </div>;
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+    return (
+        <div className="todos">
+            {todos.map((todo) => (
+                <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+            ))}
+        </div>
+    );
 };
 
 export default TodoList;
