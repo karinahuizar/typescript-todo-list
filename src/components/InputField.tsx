e755f71ba375b5ec3902c588e961a0576674d3d0
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 11:25:22 -0500

    Added an onChange to change the state.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -9,7 +9,7 @@ interface Props {
 const InputField = ({ todo, setTodo }: Props) => {
 
     return <form className='input'>
-        <input type='input' placeholder='Enter a task!' className='input__box' />
+        <input type='input' value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter a task!' className='input__box' />
         <button className='input_submit' type="submit">Go!</button>
     </form>;
 };
