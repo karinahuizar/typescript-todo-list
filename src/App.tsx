77a0256f98f9ec2716af8912919af357d320bad4
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 13:47:45 -0500

    Created a state for the todos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
 
   console.log(todo);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
