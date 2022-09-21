2b18deca834344a8493f8b93d9ba2b6314c12348
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 11:16:04 -0500

    Gave the InputField a type of Props.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 import InputField from './components/InputField';
 
 const App: React.FC = () => {
-  const [todo, useTodo] = useState<string>("");
+  const [todo, setTodo] = useState<string>("");
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
