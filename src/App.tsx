51ebda1bfa80de28aae1d0df4161245c8b306fa5
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 14:25:56 -0500

    Added a useRef hook to fix the background shadow.

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,10 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList /> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
@@ -9,8 +9,15 @@ interface Props {
 
 const InputField = ({ todo, setTodo, handleAdd }: Props) => {
 
-    return <form className='input' onSubmit={handleAdd}>
-        <input type='input'
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    return <form className='input' onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+    }}>
+        <input
+            type='input'
+            ref={inputRef}
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
             placeholder='Enter a task!'
