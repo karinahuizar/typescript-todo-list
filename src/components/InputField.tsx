071944fefaa2d0efca060aa1d5d4a83d1277dff0
Karina Huizar <karinahuizar@gmail.com>
2022-09-21 10:41:45 -0500

    Added an input form.

---
+++ src/components/InputField.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputField = () => {
+    return <form className='input'>
+        <input type='input' placeholder='Enter a task!' className='input_box' />
+    </form>;
+};
+
+export default InputField;
