73213a83cb141a9a987f023f45c50acb0f10e42d
Karina Huizar <karinahuizar@gmail.com>
2022-09-22 09:12:05 -0500

    Added functionality to the delete icon.

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -16,6 +16,10 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     const handleDone = (id: number) => {
         setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo));
     };
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
     return (
         <form className="todos__single">
             {
@@ -33,7 +37,7 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
                 <span className="icon">
                     <AiFillEdit />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDelete(todo.id)}>
                     <AiFillDelete />
                 </span>
                 <span className="icon" onClick={() => handleDone(todo.id)}>
